3e6190f36a386ff472186e8eb065cf9aa1017bc6
monsterlurks <sadzombie@mail.ru>
2016-08-22 23:50:24 +0300

    Добавила удаление по клику и перерасчет суммы

--- js/basket.js
+++ js/basket.js
@@ -12,6 +12,7 @@ function takeToBusket(){
          + card[i].price + '</div><div class="catalog_basket__close">' + imgClose + '</div>'
     }
      basketList.innerHTML+= content;
+     SumTotal();
   }
 
 for (var i = 0; i < toCard.length; i++){
@@ -23,16 +24,26 @@ for (var i = 0; i < toCard.length; i++){
      card.push({
             price: parseInt(price), //при клике пушатся в массив, потом при вызове ф-ии подгруж. в basketList
             title: title
-
         });//конец метода push()
     takeToBusket();//во время клика вызывается ф-ия
     });//конец события
 
 }
+ function SumTotal(){
+   var basketLine = document.querySelectorAll('.catalog_basket__line');
+   var sum = 0;
+   for (var i = 0; i < basketLine.length; i++){
+    var itemPrice = Number(basketLine[i].querySelector('.catalog_basket__price').innerHTML);
+       sum += itemPrice
+       }
+       document.querySelector('.catalog_basket__summ_text').innerHTML = sum;
+ }
 
-var imgCloseAll = document.querySelectorAll('.catalog_basket__close');
-for (var i = 0; i < imgCloseAll.length; i++) {
-   imgCloseAll[i].addEventListener('click', function(e){
-    e.target.closest('.catalog_basket__line').remove();
-   });
-}
+    basketList.onclick = function(e){ //повесила событие на весь контейнер с добавленными товарами
+    var target = e.target; //где был клик?
+
+    if (target.tagName == 'svg') { // если таргетнутый элемент svg, то удаляем
+       target.closest('.catalog_basket__line').remove();
+    }
+    SumTotal()
+};
